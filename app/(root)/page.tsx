006503af209c55639e6d19954309cae0fa14df7a
Nat Silprasert <natsilp@Nats-MacBook-Pro.local>
2025-06-17 13:42:57 +0700

    First Commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import SearchForm from '../../components/SearchForm'
+
+const Home = async ({ searchParams }: {
+  searchParams: Promise<{ query?: string }>;
+}) => {
+
+  const query = (await searchParams).query;
+
+  return (
+    <>
+      <section className='pink_container'>
+        <h1 className='heading'>Pitch Your Startup, <br /> Connect with entrepreneurs</h1>
+        <p className='sub-heading !max-w-3xl'>Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
+
+        <SearchForm query={query}/>
+      </section>
+    </>
+  )
+}
+
+export default Home
